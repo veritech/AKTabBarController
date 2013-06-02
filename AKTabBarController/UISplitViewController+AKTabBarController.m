e579089b2936cf23a52670ff337481fd261984e2
Jonathan Dalrymple <jonathan@float-right.co.uk>
2013-06-02 16:56:30 -0500

    Imported the header

--- AKTabBarController/UISplitViewController+AKTabBarController.m
+++ AKTabBarController/UISplitViewController+AKTabBarController.m
@@ -20,6 +20,8 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+#import "UISplitViewController+AKTabBarController.h"
+
 @class UIViewController;
 
 @implementation UISplitViewController (AKTabBarController)
